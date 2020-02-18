6e74be65888819bb632bb91847f1ec9d2d0845a7
Daniel Sitek <dan.sitek@gmail.com>
2020-02-18 13:41:58 +0100

    added readme, added language support, updated builder

---
+++ README.md
@@ -0,0 +1,19 @@
+# Tateru CLI
+
+Simple CLI static site builder tool based on Twig.
+
+## Install
+
+Install Tateru CLI in your project.
+
+```
+$ npm i -D tateru-cli
+```
+
+## Usage
+
+Create config file `config.json` with all settings based on example config file in `/test`. And run following command.
+
+```
+npx tateru-cli config.json
+```
--- index.js
+++ index.js
@@ -14,40 +14,51 @@ const _merge = lodash.merge;
 
 const cli = meow(`
 Usage:
-  ./bin/build.js [CONFIG FILE] [OPTIONS] [ARGS]
+\tnpx tateru-cli [CONFIG FILE] [OPTIONS] [ARGS]
 
 Options:
-      --env         Set build environment. (Default is dev)
-      --page  -p    Build only single page from config.
-  -h, --help        Display help and usage details
+\t--env     \tSet build environment - dev or prod. Default is dev.
+\t--page  -p\tBuild only single page from config.
+\t--lang  -l\tSelect language subset to build
+\t--help    \tDisplay help and usage details
 `, {
     flags: {
         env: {
             type: 'string',
-            default: 'dev',
+            default: '',
             description: 'Set build environment.'
         },
         page: {
             type: 'string',
             default: '',
             alias: 'p',
-            description: 'Build only single page from config'
+            description: 'Build only single page from config.'
+        },
+        lang: {
+            type: 'string',
+            default: '',
+            alias: 'l',
+            description: 'Select language subset to build.'
         }
     }
 });
 
+const ENV_DEVELOPMENT = 'dev';
+const ENV_PRODUCTION = 'prod';
+const LANG_DEFAULT = 'cs';
+
 const options = {
     configFile: cli.input[0] || 'config.json',
-    env: cli.flags.env,
-    lang: 'cs',
+    env: cli.flags.env || ENV_DEVELOPMENT,
+    lang: cli.flags.lang || LANG_DEFAULT,
 };
 
 if (process.env.NODE_ENV) {
     if (process.env.NODE_ENV === 'development') {
-        options.env = 'dev';
+        options.env = ENV_DEVELOPMENT;
     }
     if (process.env.NODE_ENV === 'production') {
-        options.env = 'prod';
+        options.env = ENV_PRODUCTION;
     }
 }
 
@@ -67,19 +78,30 @@ try {
     configFile = require(configFileSrc);
 } catch (e) {
     console.error(`Cannot load config file "${configFileSrc}"`)
+    process.exit(1);
 }
 
 const hrefData = {};
-Object.keys(configFile.pages.cs).forEach(item => {
-    hrefData[item] = `/${configFile.pages.cs[item].ext}`;
-});
+try {
+    Object.keys(configFile.pages[options.lang]).forEach(item => {
+        try {
+            hrefData[item] = `/${configFile.pages[options.lang][item].ext}`;
+        } catch (e) {
+            console.error(`Cannot find page ext in lang group "${options.lang}".`);
+        }
+    });
+} catch (e) {
+    console.error(`Cannot find pages in lang group "${options.lang}".`);
+    process.exit(1);
+}
+
 
 const renderOptions = _merge(
     configFile.options.data,
     configFile.env[options.env].data,
     {
         href: hrefData,
-        lang: 'cs'
+        lang: options.lang
     }
 );
 
@@ -90,8 +112,13 @@ const renderExtDir = path.resolve(rootDir, configFile.options.ext);
 /**
  * Translations
  */
-const translationFile = fs.readFileSync(path.resolve(rootDir, configFile.translations.cs.src));
-const translations = JSON.parse(translationFile.toString());
+const translationFilePath = path.resolve(rootDir, configFile.translations[options.lang].src);
+if (!fs.existsSync(translationFilePath)) {
+    console.error(`Cannot find translation file on path "${translationFilePath}"`);
+    process.exit(1);
+}
+const translationFileContent = fs.readFileSync(translationFilePath);
+const translations = JSON.parse(translationFileContent.toString());
 
 Twig.extendFunction('trans', (value) => {
     return _get(translations, value) || value;
@@ -123,19 +150,18 @@ Twig.extendFilter('sort_by', (value, key) => {
  * @param {String} pageName Page name in config.json file.
  */
 const renderPage = (pageName) => {
-    if (!configFile.pages.cs[pageName]) {
+    if (!configFile.pages[options.lang][pageName]) {
         console.error(`"${pageName}" were not found in config file.`);
         process.exit(1);
         return false;
     }
 
-    const buildPageConfig = configFile.pages.cs[pageName];
+    const buildPageConfig = configFile.pages[options.lang][pageName];
     const pathToExt = path.resolve(renderExtDir, buildPageConfig.ext);
     const pathToSrc = path.resolve(renderSrcDir, buildPageConfig.src);
     const relativeFileExt = path.join(configFile.options.ext, buildPageConfig.ext);
     const relativeFileSrc = path.join(configFile.options.src, buildPageConfig.src);
 
-    // TODO: test jestli byl readFileSync success.
     if (!fs.existsSync(pathToSrc)) {
         console.error(`File "${pathToSrc}" does not exits`);
         process.exit(1);
@@ -199,17 +225,38 @@ const renderPipeline = (page) => {
 }
 
 const run = () => {
-    console.log(`Environment:\t${options.env}`);
+    try {
+        console.log(`Environment:\t${options.env}`);
 
-    if (cli.flags.page) {
-        renderPipeline(cli.flags.page);
-    } else {
-        Object.keys(configFile.pages.cs).forEach(item => {
-            renderPipeline(item);
-        });
+        if (cli.flags.page && cli.flags.lang) {
+            // Build single page in selected lang
+            console.log(`Going to build page "${cli.flags.page}" in "${cli.flags.lang}" group.`)
+            renderPipeline(cli.flags.page);
+        } else if (cli.flags.page) {
+            console.log(`Going to build page "${cli.flags.page}" in "${options.lang}" group.`)
+            renderPipeline(cli.flags.page);
+        } else if (cli.flags.lang) {
+            // Build all pages in single lang
+            console.log(`Going to build pages in "${cli.flags.lang}" group.`)
+            Object.keys(configFile.pages[options.lang]).forEach(item => {
+                renderPipeline(item);
+            });
+        } else {
+            // Build everything
+            console.log(`Going to build all pages in all groups.`)
+            Object.keys(configFile.pages).forEach(lang => {
+                Object.keys(configFile.pages[lang]).forEach(item => {
+                    renderPipeline(item);
+                });
+            });
+
+        }
+
+        process.exit(0);
+    } catch (e) {
+        console.error(e);
+        process.exit(1);
     }
-
-    process.exit(0);
 }
 
 module.exports = run;
