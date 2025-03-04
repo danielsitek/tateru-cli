884639d64772f9a63f5dd4e8f4658cffdf63f52b
Daniel Sitek <dan.sitek@gmail.com>
2025-03-04 08:37:51 +0100

    fixed a few types

--- src/core/services/buildTemplate.ts
+++ src/core/services/buildTemplate.ts
@@ -2,20 +2,21 @@ import { prepareTwigConfiguration } from './twig/prepareTwigConfiguration';
 import { twigServiceRender } from './twig/twigService';
 
 /**
+ * Build twig template with provided data.
  *
- * @param data Object - JSON data with twig variables.
- * @param translation Object - JSON data with translation, used in trans() function.
+ * @param renderData Object - JSON data with twig variables.
+ * @param translations Object - JSON data with translation, used in trans() function.
  * @param templateBase String - path to base dir with twig template files, usually src/twig.
  * @param templateFile String - path to twig template, usually src/twig/page.html.twig
  */
 export const buildTemplate = (
-    data: any,
-    translation: any,
+    renderData: Record<string, unknown>,
+    translations: Record<string, unknown>,
     templateBase: string,
     templateFile: string,
 ): string => {
     const fileTwigConfig = prepareTwigConfiguration(templateFile, templateBase);
-    const template = twigServiceRender(fileTwigConfig, data, translation);
+    const template = twigServiceRender(fileTwigConfig, renderData, translations);
 
     return template;
 };
--- src/core/utils/composeData.ts
+++ src/core/utils/composeData.ts
@@ -8,12 +8,12 @@ export const composeData = (
     configEnvData: ConfigFile['env'][Environment]['data'],
     configPageData: ConfigFile['pages'][string][string]['data'],
     configPages: ConfigFile['pages'][string],
-) => {
+): Record<string, unknown> => {
     const href = getHrefData(
         configPages, // config.pages.cs
     );
 
-    const data = deepMerge(
+    const data = deepMerge<Record<string, unknown>>(
         {
             ...configOptionsData, // config.options.data
         },
